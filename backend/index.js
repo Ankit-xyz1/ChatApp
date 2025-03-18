ccf7027a6f45af8267b63e1406c9851f2a60c1e4
Ankit <86563541+Ankit-xyz1@users.noreply.github.com>
2025-03-18 18:19:48 +0530

    Update index.js
    cors updation

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials:true,
-    origin:"http://localhost:5173",
+    origin:"https://caht-app-backend.vercel.app/",
 }))
 
 const __dirname = path.resolve();
